2ceeaa2d09a3af9ef9d0ef78620af1bd56ebc29e
Muhammad Manan Nasir <mannannasir49@gmail.com>
2023-07-23 10:06:40 +0500

    Added functionality of filter and pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,11 +16,17 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 // Get All Products
 
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const apiFeature = new ApiFeatures(Product.find(), req.query).search();
+  const resultPerPage = 10;
+  const productCount = await Product.countDocuments;
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
   const products = await apiFeature.query;
   res.status(200).json({
     success: true,
     products,
+    productCount,
   });
 });
 
